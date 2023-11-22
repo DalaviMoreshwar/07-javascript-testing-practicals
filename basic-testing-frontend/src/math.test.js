3339b019986e2b904c3e4620a7ab2f1d8f327b19
Moreshwar Dalvi <moreshwardlv@gmail.com>
2023-11-22 23:14:11 +0530

    more tests

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -15,3 +15,39 @@ it("should summarize all number values in an array", () => {
   );
   expect(result).toBe(expectedResult);
 });
+
+it("should yeild NaN if at least one invalid number is provided", () => {
+  const numbers = ["invalid", 1];
+
+  const result = add(numbers);
+
+  expect(result).toBeNaN();
+});
+
+it("should yeild a correct sum if an array of numeric string values is provided", () => {
+  const numbers = ["1", "2"];
+
+  const result = add(numbers);
+
+  const expectedResult = numbers.reduce(
+    (prevVal, currentVal) => +prevVal + +currentVal,
+    0
+  );
+  expect(result).toBe(expectedResult);
+});
+
+it("should yeild 0 if an empty array is provided", () => {
+  const numbers = [];
+
+  const result = add(numbers);
+
+  expect(result).toBe(0);
+});
+
+it("should throw an error if no values are passed into the function", () => {
+  const resultFn = () => {
+    add();
+  };
+
+  expect(resultFn).toThrow();
+});
